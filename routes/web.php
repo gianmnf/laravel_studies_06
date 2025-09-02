04d1a468bf5e618a16088c6d64e2d6b50c733cae
Gian Michel <gianmnf@gmail.com>
2025-09-02 17:50:39 -0300

    001

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/','home');
+Route::get('/test', function(){
+    $products = Product::all();
+    echo '<pre>';
+    print_r($products->toArray());
+    echo '</pre>';
+});
