1b64e4d2e73b2f5bd25f44417789df2ad2b76737
Gian Michel <gianmnf@gmail.com>
2025-09-05 13:29:50 -0300

    004

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,16 +2,49 @@
 
 namespace App\Http\Controllers;
 
-// use App\Models\Product;
-
-use App\Models\TestModel;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index() {
-        $results = TestModel::all()->toArray();
+        // get all the products data
+        // $results = Product::all();
+
+        // $results = Product::all()->toArray();
+
+        // return the array results as an stdClass objects array
+        //$results = $this->ArrayOfObjects(Product::all()->toArray());
+        //$this->showData($results);
+
+        // get products ordered by name
+        /* $results = Product::orderBy('product_name')
+                            ->get()
+                            ->toArray(); */
+
+        // get the first three products
+        // $results = Product::limit(3)->get()->toArray();
+        // $this->showData($results);
+
+        // find by id
+        $results = Product::find(10)->toArray();
+        $this->showData($results);
+    }
+
+    private function showData($data)
+    {
         echo '<pre>';
-        print_r($results);
+        print_r($data);
+    }
+
+    private function ArrayOfObjects($data)
+    {
+        $tmp = [];
+        foreach($data as $key => $value)
+        {
+            $tmp[] = (object) $value;
+        }
+
+        return $tmp;
     }
 }
